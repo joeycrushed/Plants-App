8bf909b2b981e88b898b1537037b6e31b8a5efb2
yonatan rospigliosi <jonrospi@hotmail.com>
2021-06-01 16:55:30 +0100

    create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+jon = User.first
+plants = ["Bush", "Flower", "Tree", "Rose"]
+10.times do
+  plant = Plant.new(
+    name: "#{Faker::Creature::Animal.name} #{plants.sample}",
+    description: Faker::TvShows::BigBangTheory.quote,
+    image: "https://source.unsplash.com/featured/?plant"
+  )
+  plant.user = jon
+  plant.save!
+end

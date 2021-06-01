3bcbfcfbc374315700dfb2458eea3e0bad0aa2be
yonatan rospigliosi <jonrospi@hotmail.com>
2021-06-01 12:21:03 +0100

    added validations

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,4 +1,7 @@
 class Plant < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  validates :name, uniqueness: true, presence: true
+  validates :image, presence: true
+  validates :description, presence: true
 end

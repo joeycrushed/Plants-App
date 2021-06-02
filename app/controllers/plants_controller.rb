320ea71446a7914d42a22ec45eaad87fb451180c
Robyn Ellison <robyn.victoria.ellison@gmail.com>
2021-06-02 11:48:23 +0100

    create plant method fixed

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -10,6 +10,7 @@ class PlantsController < ApplicationController
 
   def create
     @plant = Plant.new(plant_params)
+    @plant.user = current_user
     if @plant.save
       redirect_to plant_path(@plant)
     else

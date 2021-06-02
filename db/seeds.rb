d9e304a44d159bc1f3ea8ac66063b8b9cb03d428
Robyn Ellison <robyn.victoria.ellison@gmail.com>
2021-06-02 14:51:22 +0100

    plant images to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Booking.destroy_all
+Plant.destroy_all
 jon = User.first
 plants = ["Bush", "Flower", "Tree", "Rose"]
-10.times do
+images = ["https://images.unsplash.com/photo-1587168574161-c05a6281fab4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
+ "https://images.unsplash.com/photo-1475541148680-00e4fb86ee25?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80",
+ "https://images.unsplash.com/photo-1467043198406-dc953a3defa0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2366&q=80",
+ "https://images.unsplash.com/photo-1599327285923-8c887e823734?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
+ "https://images.unsplash.com/photo-1599009944997-3544a939813c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2734&q=80",
+ "https://images.unsplash.com/photo-1542728928-0011f81446e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80",
+ "https://images.unsplash.com/photo-1584643120617-3f14f7729763?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
+ "https://images.unsplash.com/photo-1596521731523-42719bff9ed9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2775&q=80",
+ "https://images.unsplash.com/photo-1502651492682-6c7b46d75d85?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2734&q=80",
+ "https://images.unsplash.com/photo-1517024785852-498ab0c02de5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1342&q=80",
+ "https://images.unsplash.com/photo-1519336056116-bc0f1771dec8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2734&q=80"
+]
+9.times do
   plant = Plant.new(
     name: "#{Faker::Creature::Animal.name} #{plants.sample}",
     description: Faker::TvShows::BigBangTheory.quote,
-    image: "https://source.unsplash.com/featured/?plant"
+    image: images.sample
   )
   plant.user = jon
   plant.save!

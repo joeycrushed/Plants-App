19133d5b72cde88a913e91bda685c6aa143d828a
yonatan rospigliosi <jonrospi@hotmail.com>
2021-06-03 17:37:47 +0100

    change color footer and navbar and hover social media icons

--- db/schema.rb
+++ db/schema.rb
@@ -47,9 +47,9 @@ ActiveRecord::Schema.define(version: 2021_06_03_115701) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
     t.string "first_name"
     t.string "last_name"
-    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

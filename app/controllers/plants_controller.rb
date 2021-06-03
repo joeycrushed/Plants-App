87373d0736d06d7b4e8a0a52a370f685ca8b2a9b
Robyn Ellison <robyn.victoria.ellison@gmail.com>
2021-06-03 17:04:11 +0100

    plant search created

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -3,7 +3,11 @@ class PlantsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @plants = Plant.all
+    if params[:query].present?
+      @plants = Plant.search_by_name_and_description("#{params[:query]}")
+    else
+      @plants = Plant.all
+    end
   end
 
   def new
